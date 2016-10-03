90c7a6597fbfc566a13543d14e18fe2a088ddce7
Lucas Lentz <lucas.lentz@hotmail.com>
2016-10-03 18:31:29 -0300

    heroku configs url

--- configuration/config.js
+++ configuration/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"facebook_api_key"	  : process.env.APP_KEY || "",
 	"facebook_api_secret" : process.env.APP_SECRET || "",
-	"callback_url"		  : "http://localhost:3000/auth/facebook/callback",
+	"callback_url"		  : process.env.APP_URL + "auth/facebook/callback" || "http://localhost:3000/auth/facebook/callback",
 	"use_database"		  : "false",
 	"host"				  : "localhost",
 	"username"			  : "root",
