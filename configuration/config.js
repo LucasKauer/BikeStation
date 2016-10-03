d3511166453628d47b06b23f9450e1ca507b218a
Lucas Lentz <lucas.lentz@hotmail.com>
2016-10-03 18:14:32 -0300

    Heroku stuff part III.

--- configuration/config.js
+++ configuration/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	"facebook_api_key"	  : "",
-	"facebook_api_secret" : "",
+	"facebook_api_key"	  : APP_KEY || "",
+	"facebook_api_secret" : APP_SECRET || "",
 	"callback_url"		  : "http://localhost:3000/auth/facebook/callback",
 	"use_database"		  : "false",
 	"host"				  : "localhost",
