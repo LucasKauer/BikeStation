6668bf386ccfd3e0d93670f060bf034b3bae1220
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2016-10-04 14:02:36 -0300

    versionando seila

--- configuration/config.js
+++ configuration/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	"facebook_api_key"	  : "",
-	"facebook_api_secret" : "",
+	"facebook_api_key"	  : "202459236832145",
+	"facebook_api_secret" : "937cc6e7849cb5ebacea01ac54210b8f",
 	"callback_url"		  : "http://localhost:3000/auth/facebook/callback",
 	"use_database"		  : "false",
 	"host"				  : "localhost",
