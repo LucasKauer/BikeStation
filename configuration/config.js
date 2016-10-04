c33ba621cb386716b933131f2ed0ac3791978242
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2016-10-04 14:47:48 -0300

    fix(merge): corrigir o merge da última alteração.

--- configuration/config.js
+++ configuration/config.js
@@ -1,13 +1,7 @@
 module.exports = {
-<<<<<<< HEAD
-	"facebook_api_key"	  : "202459236832145",
-	"facebook_api_secret" : "937cc6e7849cb5ebacea01ac54210b8f",
-	"callback_url"		  : "http://localhost:3000/auth/facebook/callback",
-=======
 	"facebook_api_key"	  : process.env.APP_KEY || "",
 	"facebook_api_secret" : process.env.APP_SECRET || "",
 	"callback_url"		  : process.env.APP_URL || "http://localhost:3000/auth/facebook/callback",
->>>>>>> 406b1bcba5230a41614d5c4287822db996a4d2e2
 	"use_database"		  : "false",
 	"host"				  : "localhost",
 	"username"			  : "root",
